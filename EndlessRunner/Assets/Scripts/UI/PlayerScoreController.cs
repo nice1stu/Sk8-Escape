980bed890ea429a5908595a5a33dc72791315ff2
Jacob-Roming <jacob.roming@student.forsbergsskola.se>
2023-04-24 11:58:34 +0200

    Creates a dummy player and makes the score go up the further the player travels

--- EndlessRunner/Assets/Scripts/UI/PlayerScoreController.cs
+++ EndlessRunner/Assets/Scripts/UI/PlayerScoreController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private float oldPlayerPosition;
+    private GameObject player;
+    private PlayerScoreModel model;
+    private PlayerScoreView view;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        oldPlayerPosition = player.transform.position.x;
+        model = gameObject.GetComponent<PlayerScoreModel>();
+        view = gameObject.GetComponent<PlayerScoreView>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Get player position
+        float newPlayerPosition = player.transform.position.x;
+        model.AddToScore(newPlayerPosition - oldPlayerPosition);
+
+        oldPlayerPosition = newPlayerPosition;
+
     }
 }
--- EndlessRunner/Assets/Scripts/UI/PlayerScoreModel.cs
+++ EndlessRunner/Assets/Scripts/UI/PlayerScoreModel.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerScoreModel : MonoBehaviour
 {
+    private double score;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,25 @@ public class PlayerScoreModel : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetScore(double newScore)
+    {
+        score = newScore;
+    }
+
+    public void AddToScore(double toAdd)
+    {
+        score += toAdd;
+    }
+    
+    public void AddToScore(float toAdd)
+    {
+        score += toAdd;
+    }
+
+    public double GetScore()
+    {
+        return score;
     }
 }
