9fd5e449b8eed01af9eb0e3ecb5b590c896f03ae
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-04-25 15:33:53 +0200

    #500 Adding Sprite field

--- EndlessRunner/Assets/Inventory/Scripts/BaseLootBox.cs
+++ EndlessRunner/Assets/Inventory/Scripts/BaseLootBox.cs
@@ -4,6 +4,8 @@ namespace Inventory.Scripts
 {
     public class BaseLootBox : MonoBehaviour
     {
+        public Sprite icon; 
+        
         //being able to open the loot box
        public void OpenLootBox()
        {
