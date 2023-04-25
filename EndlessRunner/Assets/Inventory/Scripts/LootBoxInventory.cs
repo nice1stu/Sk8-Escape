fc1e160176ce0fc12b7ae0e286a8937269472838
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-04-25 16:09:50 +0200

    #500 Testing AddLootBox function

--- EndlessRunner/Assets/Inventory/Scripts/LootBoxInventory.cs
+++ EndlessRunner/Assets/Inventory/Scripts/LootBoxInventory.cs
@@ -4,6 +4,21 @@ namespace Inventory.Scripts
 {
     public class LootBoxInventory : MonoBehaviour
     {
+        [ContextMenu("Add Loot Box")]
+
+        void AddLootBox()
+        {
+            foreach (var slot in lootBoxSlots)
+            {
+                if (slot == null)
+                {
+                    BaseItem.coins++;
+                    Debug.Log($"Coins added, {BaseItem.coins}");
+                    return;
+                }
+            }
+        }
+        
         public BaseLootBox[] lootBoxSlots = new BaseLootBox[4];
     }
 }
