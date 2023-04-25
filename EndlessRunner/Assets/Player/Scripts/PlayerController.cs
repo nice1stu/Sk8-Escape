bbcf61bf99233e4ed71e1ed6a27a0dbcab525b05
M1crob1ology <luca.belluso@elev.ga.lbs.se>
2023-04-25 17:13:30 +0200

    Did some progress on the movement

--- EndlessRunner/Assets/Player/Scripts/PlayerController.cs
+++ EndlessRunner/Assets/Player/Scripts/PlayerController.cs
@@ -27,6 +27,11 @@ public class PlayerController : MonoBehaviour
     private bool _grounded;
     private bool _canGrind;
 
+    public bool IsFalling
+    {
+        get { return _rb.velocity.y < 0.01f; }
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -38,36 +43,102 @@ public class PlayerController : MonoBehaviour
         currentState = SkateboardTrickState.Coast;
     }
 
+    private bool upSwipe, downSwipe, leftSwipe, rightSwipe, press;
+
+
+    private void Update()
+    {
+        GetInputs(true);
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
+        Debug.Log(currentState);
+        GetInputs(true);
+        _grounded = GetGrounded();
         ConstantMove();
         DummyInputHandling();
+        GetInputs(false);
+    }
+
+    private void GetInputs(bool get)
+    {
+        // tihihi
+        if (upSwipe != get)
+            upSwipe = Input.GetKeyDown(KeyCode.UpArrow);
+        if (downSwipe != get)
+            downSwipe = Input.GetKeyDown(KeyCode.DownArrow);
+        if (leftSwipe != get)
+            leftSwipe = Input.GetKeyDown(KeyCode.LeftArrow);
+        if (rightSwipe != get)
+            rightSwipe = Input.GetKeyDown(KeyCode.RightArrow);
+        if (press != get)
+            press = Input.GetKeyDown(KeyCode.Space);
     }
 
     private void DummyInputHandling()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (upSwipe)
         {
-            if (currentState == SkateboardTrickState.Coast)
+            if (CanOllie())
+            {
                 Ollie();
-            if (currentState == SkateboardTrickState.Ollie)
+                return;
+            }
+
+            if (CanKickflip())
+            {
                 Kickflip();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            if (currentState == SkateboardTrickState.Coast)
-                Shuvit();
+                return;
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (CanCoast())
+        {
+            currentState = SkateboardTrickState.Coast;
+            return;
+        }
+
+        if (CanFall())
+        {
+            currentState = SkateboardTrickState.Falling;
+            return;
+        }
+        
+        
+        return;
+        if (currentState != SkateboardTrickState.Falling && IsFalling) // Makes it so it only changes the state once.
+            currentState = SkateboardTrickState.Falling;
+        else if (currentState != SkateboardTrickState.Coast && _grounded)
+            currentState = SkateboardTrickState.Coast;
+
+        if (upSwipe)
+        {
+            if (currentState == SkateboardTrickState.Coast && currentState != SkateboardTrickState.Ollie)
+            {
+                Ollie();
+                return;
+            }
+            else if (currentState == SkateboardTrickState.Ollie && currentState != SkateboardTrickState.Kickflip)
+            {
+                Kickflip();
+                return;
+            }
+        }
+
+        if (rightSwipe)
+        {
+            if (currentState == SkateboardTrickState.Coast) Shuvit();
+        }
+
+        if (downSwipe)
         {
             if (currentState == SkateboardTrickState.Coast)
                 StartCoroutine(Coffin());
         }
-        
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (press)
         {
             if (_canGrind)
                 Grind();
@@ -79,44 +150,93 @@ public class PlayerController : MonoBehaviour
         _rb.velocity = new Vector2(_model.movementSpeed * Time.deltaTime, _rb.velocity.y);
     }
 
+    private void AddToCurrentVelocity(Vector2 addedVelocity)
+    {
+        Vector3 vel = _rb.velocity + addedVelocity;
+        _rb.velocity = new Vector2(vel.x, Mathf.Clamp(vel.y, addedVelocity.y, _model.maxJumpVelocity));
+    }
+
+    // Assuming it's checked after all the other tricks
+    private bool CanCoast()
+    {
+        return _grounded && _rb.velocity.y <= -Mathf.Epsilon; // epsilon is a "really tiny number" // leo
+    }
+
+    private bool CanFall()
+    {
+        return (!_grounded && _rb.velocity.y < 0);
+    }
+
+    private bool CanOllie()
+    {
+        if (!_grounded) return false;
+
+        if (currentState == SkateboardTrickState.Coast) return true;
+
+        return false;
+    }
+
     private void Ollie()
     {
+        Debug.Log("Ollie");
         currentState = SkateboardTrickState.Ollie;
-        _rb.velocity = new Vector2(_rb.velocity.x, _model.ollieJumpForce);
+        AddToCurrentVelocity(Vector2.up * _model.ollieJumpForce);
         _grounded = false;
     }
-    
+
+    private bool CanKickflip()
+    {
+        if (_grounded) return false;
+
+        if (currentState == SkateboardTrickState.Ollie) return true;
+
+        return false;
+    }
+
     private void Kickflip()
     {
+        Debug.Log("Kickflip");
         currentState = SkateboardTrickState.Kickflip;
-        _rb.velocity = new Vector2(_rb.velocity.x, _model.kickflipJumpForce);
-        _grounded = false;
+        AddToCurrentVelocity(Vector2.up * _model.kickflipJumpForce);
     }
-    
+
     private void Shuvit()
     {
+        Debug.Log("Shuvit");
         currentState = SkateboardTrickState.Shuvit;
         _rb.velocity = new Vector2(_rb.velocity.x, _model.shuvitJumpForce);
-        _grounded = false;
     }
+
     private void Grind()
     {
+        Debug.Log("Grind");
         currentState = SkateboardTrickState.Grind;
         _rb.velocity = new Vector2(_rb.velocity.x, 0);
-        _grounded = false;
     }
 
     private IEnumerator Coffin()
     {
+        Debug.Log("Coffin");
         currentState = SkateboardTrickState.Coffin;
-        _col.size = new Vector2(_col.size.x, _col.size.y/4);
+        _col.size = new Vector2(_col.size.x, _col.size.y / 4);
         yield return new WaitForSecondsRealtime(_model.coffinTime);
-        _col.size = new Vector2(_col.size.x, _col.size.y*4);
+        _col.size = new Vector2(_col.size.x, _col.size.y * 4);
     }
 
-    
-    private void OnCollisionEnter2D(Collision2D col)
+    ContactPoint2D[] _collisionBuffer = new ContactPoint2D[100];
+
+    private bool GetGrounded()
     {
-        _grounded = true;
+        int count = _col.GetContacts(_collisionBuffer);
+        for (int i = 0; i < count; i++)
+        {
+            if (LayerMask.GetMask(LayerMask.LayerToName(_collisionBuffer[i].collider.gameObject.layer)) == _model.groundLayers) continue; 
+            if (Vector2.Angle(_collisionBuffer[i].normal, Vector2.up) < _model.maxGroundAngle)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- EndlessRunner/Assets/Player/Scripts/PlayerModel.cs
+++ EndlessRunner/Assets/Player/Scripts/PlayerModel.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerModel : MonoBehaviour
 {
+    [Header("General Movement")]
     public float movementSpeed = 5f;
+    [Header("Movement Restrictions")] 
+    public float maxJumpVelocity = 30f;
+    [Header("Tricks")]
     public float ollieJumpForce = 5f;
     public float kickflipJumpForce = 3f;
     public float shuvitJumpForce = 2f;
     public float coffinTime = 1f;
+    [Header("Collisions")] 
+    public float maxGroundAngle = 45f;
     public LayerMask groundLayers;
 }
