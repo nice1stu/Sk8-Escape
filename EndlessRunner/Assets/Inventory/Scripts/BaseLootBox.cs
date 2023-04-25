dacc95d1d1b99e4f61bc11b8b6023f7137d11833
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-04-25 16:09:04 +0200

    #500 Creating timeToOpen and discardValue fields

--- EndlessRunner/Assets/Inventory/Scripts/BaseLootBox.cs
+++ EndlessRunner/Assets/Inventory/Scripts/BaseLootBox.cs
@@ -4,7 +4,9 @@ namespace Inventory.Scripts
 {
     public class BaseLootBox : MonoBehaviour
     {
-        public Sprite icon; 
+        public Sprite icon;
+        public Time timeToOpen;
+        public int discardValue;
         
         //being able to open the loot box
        public void OpenLootBox()
