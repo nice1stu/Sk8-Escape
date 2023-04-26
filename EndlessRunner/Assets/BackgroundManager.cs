9ac01d5bfb96f9f21e406cde0b314f141309dff0
KaleSmug <karl.sandegren@gmail.com>
2023-04-26 11:01:45 +0200

    Background switch places

--- EndlessRunner/Assets/BackgroundManager.cs
+++ EndlessRunner/Assets/BackgroundManager.cs
@@ -10,20 +10,17 @@ public class BackgroundManager : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log("triggerExit");
         if (other.gameObject.layer == 10)
         {
-            // Move the first background to the end of the other background
-            Vector3 endPosition = otherBackground.transform.position + Vector3.right * otherBackground.GetComponent<SpriteRenderer>().bounds.size.x;
-            transform.position = endPosition;
-
-            // Make the first background a child of the other background
-            transform.parent = otherBackground.transform;
-
+            Debug.Log("triggerExit");
+            otherBackground.transform.parent = transform.parent;
+            
             // Set the first background as the first background again
-            Vector3 startPosition = otherBackground.transform.position - Vector3.right * otherBackground.GetComponent<SpriteRenderer>().bounds.size.x;
+            Vector3 startPosition = (otherBackground.transform.position + Vector3.right * otherBackground.GetComponent<SpriteRenderer>().bounds.size.x);
             transform.position = startPosition;
-
+            
+            transform.parent = otherBackground.transform;
+            
             first = false;
             gameObject.GetComponent<BackgroundView>().enabled=false;
             otherBackground.first = true;
