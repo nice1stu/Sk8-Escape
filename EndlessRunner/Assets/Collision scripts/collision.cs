5f9cfcadde35c80d348de14485b372a64b69d71c
Mohammad Alsharaa <mohammad.al.shara@student.forsbergsskola.se>
2023-04-26 11:46:18 +0200

    camera shake and dark background tint on death

--- EndlessRunner/Assets/Collision scripts/collision.cs
+++ EndlessRunner/Assets/Collision scripts/collision.cs
@@ -3,22 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class collision : MonoBehaviour
 {
     public GameObject spawnPoint;
-    
+    public CameraShake cameraShake;
+    public Image darkBackground;
+
+    private void Start()
+    {
+        darkBackground.enabled = false;
+    }
+
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.collider.gameObject.layer == LayerMask.NameToLayer("WallObstacles"))
         {
-            Death();
+            darkBackground.enabled = true;
+            //gameObject.GetComponent<MeshRenderer>().enabled = false;
+           StartCoroutine(cameraShake.Shake(.13f,0.6f));
+           StartCoroutine(DelayCoroutine(1.0f));
+           
         }
     }
 
     void Death()
     {
+       // gameObject.GetComponent<MeshRenderer>().enabled = true;
+       darkBackground.enabled = false;
         transform.position = spawnPoint.transform.position;
+        
+    }
+    
+    IEnumerator DelayCoroutine(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        Death();
+        
     }
     
 }
