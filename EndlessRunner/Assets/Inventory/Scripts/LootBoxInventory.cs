e2d7a2d9a6f84d1baeef4ac2569ba1dde2d86e35
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-04-26 16:49:21 +0200

    #500 Adding comments

--- EndlessRunner/Assets/Inventory/Scripts/LootBoxInventory.cs
+++ EndlessRunner/Assets/Inventory/Scripts/LootBoxInventory.cs
@@ -5,34 +5,39 @@ namespace Inventory.Scripts
 {
     public class LootBoxInventory : MonoBehaviour
     {
+        //This is the fixed array that stores the loot boxes
         private static readonly BaseLootBox[] LootBoxSlots = new BaseLootBox[4];
-        private readonly List<InventorySlot> _inventorySlots = new List<InventorySlot>();
+        //This is the list that stores the inventory slots
+        private readonly List<InventorySlot> _inventorySlots = new();
 
         private void Start()
         {
+            //On start, adds inventory slots to the list
             _inventorySlots.AddRange(GetComponentsInChildren<InventorySlot>());
         }
 
+        //May be removed in a later stage, good for testing
         [ContextMenu("Add Loot Box")]
-        void Add()
+        
+        void Add()//This function is for the context menu
         {
             AddLootBox(FindObjectOfType<BaseLootBox>());
         }
 
-        void AddLootBox(BaseLootBox lootBox)
+        void AddLootBox(BaseLootBox lootBox) //Adds loot boxes to the loot box inventory
         {
             for (var i = 0; i < LootBoxSlots.Length; i++)
             {
-                if (LootBoxSlots[i] == null)
+                if (LootBoxSlots[i] == null)//Find the first empty slot
                 {
                     LootBoxSlots[i] = lootBox;
-                    _inventorySlots[i].AddLootBoxIcon(lootBox);
+                    _inventorySlots[i].AddLootBoxIcon(lootBox);//Adds the loot box image to the inventory slot
                     return;
                 }
             }
         }
 
-        public static void RemoveLootBox(int index)
+        public static void RemoveLootBox(int index)//Removes the loot box at the index(check InventorySlot script)
         {
             LootBoxSlots[index] = null;
         }
