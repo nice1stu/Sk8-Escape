d87e47608df748a2f266926a689c4688ec19e69a
Raphael McCabe <raphael.mccabe@student.forsbergsskola.se>
2023-04-27 13:14:28 +0200

    Adds Temporary Chest Sprite and Sound Effect

--- EndlessRunner/Assets/Environment/Scripts/InRunChest.cs
+++ EndlessRunner/Assets/Environment/Scripts/InRunChest.cs
@@ -1,14 +1,34 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using static RunInventoryManager;
 using UnityEngine;
 
 public class InRunChest : MonoBehaviour, IPickupable
 {
+    private AudioSource _audioSource;
+    private SpriteRenderer _spriteRenderer;
+    private Collider2D _col;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _col = GetComponent<Collider2D>();
+    }
+
     public void OnPickup()
     {
-        chestAmount++;
-        Destroy(gameObject);
+        coinAmount++;
         Debug.Log("chest amount:" + chestAmount);
+        _audioSource.Play();
+        _spriteRenderer.color = Color.clear;
+        _col.enabled = false;
+        StartCoroutine(CO_DelayedDeath());
+    }
+
+    private IEnumerator CO_DelayedDeath()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        Destroy(gameObject);
     }
 }
