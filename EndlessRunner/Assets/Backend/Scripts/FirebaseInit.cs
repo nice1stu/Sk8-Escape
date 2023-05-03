1c6a9d7c3a535191bd15696075e6c93368f49d92
nihanbir <nihan.bir@gmail.com>
2023-05-03 11:13:29 +0200

    Update FirebaseInit.cs

--- EndlessRunner/Assets/Backend/Scripts/FirebaseInit.cs
+++ EndlessRunner/Assets/Backend/Scripts/FirebaseInit.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class FirebaseInit : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private FirebaseApp app;
     void Start()
     {
-        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
-            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available) {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                app = FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            } else {
+                Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}");
+                // Firebase Unity SDK is not safe to use here.
+            }
         });
+        
     }
 }
