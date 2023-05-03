6fa1fbd43a8323e36e9a782798dcb766d0162ea7
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-05-03 14:30:22 +0200

    #400 Adds comment to ads scripts

--- EndlessRunner/Assets/Ads/Scripts/AdInterstitialDisplay.cs
+++ EndlessRunner/Assets/Ads/Scripts/AdInterstitialDisplay.cs
@@ -5,20 +5,27 @@ namespace Ads.Scripts
 {
     public class AdInterstitialDisplay : MonoBehaviour
     {
+        //The game id to access ads
         public string myGameIdAndroid = "5266579";
+        //The type of ad
         public string adUnitIdAndroid = "Interstitial_Android";
+        //Ad id that is being used
         public string myAdUnitId;
+        //Keep track if ad has started
         public bool adStarted;
+        //If you want to be in test mode or not
         public bool testMode = true;
 
         private void Start()
         {
+            //Initialize the ad
             Advertisement.Initialize(myGameIdAndroid, testMode);
             myAdUnitId = adUnitIdAndroid;
         }
 
         private void Update()
         {
+            //Run the ad
             if (Advertisement.isInitialized && !adStarted)
             {
                 Advertisement.Load(myAdUnitId);
