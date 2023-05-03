1421b83668e9befaf003ae6edea6656db1e5f38b
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-05-03 13:40:56 +0200

    #400 Adds Ad Interstitial script

---
+++ EndlessRunner/Assets/Ads/Scripts/AdInterstitialDisplay.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+namespace Ads.Scripts
+{
+    public class AdInterstitialDisplay : MonoBehaviour
+    {
+        public string myGameIdAndroid = "5266579";
+        public string adUnitIdAndroid = "Interstitial_Android";
+        public string myAdUnitId;
+        public bool adStarted;
+        public bool testMode = true;
+
+        private void Start()
+        {
+            Advertisement.Initialize(myGameIdAndroid, testMode);
+            myAdUnitId = adUnitIdAndroid;
+        }
+
+        private void Update()
+        {
+            if (Advertisement.isInitialized && !adStarted)
+            {
+                Advertisement.Load(myAdUnitId);
+                Advertisement.Show(myAdUnitId);
+                adStarted = true;
+            }
+        }
+    }
+}
