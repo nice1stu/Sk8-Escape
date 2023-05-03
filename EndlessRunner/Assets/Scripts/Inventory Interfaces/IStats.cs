1f68c5509187f87ede57a40e432da38d6cdba69d
Hampus brink <112471046+Gandhopp@users.noreply.github.com>
2023-05-03 14:59:03 +0200

    IStats interface

---
+++ EndlessRunner/Assets/Scripts/Inventory Interfaces/IStats.cs
@@ -0,0 +1,10 @@
+namespace Inventory_Interfaces
+{
+    public interface IStats
+    {
+        public int Speed { get; set; }
+        public int Stability { get; set; }
+        public int Style { get; set; }
+        public int Balance { get; set; }
+    }
+}
