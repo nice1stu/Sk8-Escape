6718b00c75f2aa6e37a546b67827de4e7156ee8a
nihanbir <nihan.bir@gmail.com>
2023-05-03 13:44:48 +0200

    Revert "Update FirebaseInit.cs"
    This reverts commit 1c6a9d7c3a535191bd15696075e6c93368f49d92.

--- EndlessRunner/Assets/Backend/Scripts/FirebaseInit.cs
+++ EndlessRunner/Assets/Backend/Scripts/FirebaseInit.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 
 public class FirebaseInit : MonoBehaviour
 {
-    private FirebaseApp app;
+    // Start is called before the first frame update
     void Start()
     {
-        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
-            var dependencyStatus = task.Result;
-            if (dependencyStatus == DependencyStatus.Available) {
-                // Create and hold a reference to your FirebaseApp,
-                // where app is a Firebase.FirebaseApp property of your application class.
-                app = FirebaseApp.DefaultInstance;
-
-                // Set a flag here to indicate whether Firebase is ready to use by your app.
-            } else {
-                Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}");
-                // Firebase Unity SDK is not safe to use here.
-            }
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
         });
-        
     }
 }
