6bb4ee843f76741320c1d187c75f95164b3f907c
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-05-03 22:47:02 +0200

    #404 Settings Integration
    Add Getter and Setter for Music & Sfx settings

--- EndlessRunner/Assets/Scripts/GameTester.cs
+++ EndlessRunner/Assets/Scripts/GameTester.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 using UnityEngine.Serialization;
 
-//This script is just for testing the SaveManager script
+// This script is just for testing the SaveManager script - XXX Delete in Main Game XXX
 public class GameTester : MonoBehaviour
 {
+    public float adjustMusicVolume;
+    public bool toggleMusicMute = true;
+    public float adjustSfxVolume;
+    public bool toggleSfxMute = true;
+    
     public int totalPlayerScore;
     public int totalPlayerGems;
     public int totalPlayerCoins;
     public int currentRunScore;
     [SerializeField] private int highScore;
-    public InventoryManager inventoryManager;
+    
     private SaveManager saveManager;
 
     private void Start()
@@ -20,11 +25,11 @@ public class GameTester : MonoBehaviour
         // Load player data
         LoadPlayerData();
     }
-    
+
     void UpdateHighScore()
     {
-        if (currentRunScore <= saveManager.SavedHighScore) return;
-        saveManager.SavedHighScore = currentRunScore;
+        if (currentRunScore <= saveManager.SaveHighScore) return;
+        saveManager.SaveHighScore = currentRunScore;
     }
 
     private void SavePlayerData()
@@ -32,9 +37,14 @@ public class GameTester : MonoBehaviour
         if (saveManager == null) return;
 
         // Update the saved player points and coins in the SaveManager
-        saveManager.SavedPlayerScore = totalPlayerScore;
-        saveManager.SavedPlayerGems = totalPlayerGems;
-        saveManager.SavedPlayerCoins = totalPlayerCoins;
+        saveManager.SaveTotalScore = totalPlayerScore;
+        saveManager.SaveTotalGems = totalPlayerGems;
+        saveManager.SaveTotalCoins = totalPlayerCoins;
+
+        saveManager.MusicVolume = adjustMusicVolume;
+        saveManager.MusicMute = toggleMusicMute;
+        saveManager.SfxVolume = adjustSfxVolume;
+        saveManager.SfxMute = toggleSfxMute;
         
         // Save the player data in the SaveManager
         saveManager.SaveGameData();
@@ -46,22 +56,23 @@ public class GameTester : MonoBehaviour
         saveManager.LoadData();
 
         // Update the player points and coins from the saved data
-        totalPlayerScore = saveManager.SavedPlayerScore;
-        totalPlayerGems = saveManager.SavedPlayerGems;
-        totalPlayerCoins = saveManager.SavedPlayerCoins;
-        highScore = saveManager.SavedHighScore;
-
-        // Update the inventory from the saved data
-        if (inventoryManager == null || saveManager.inventoryManager == null) return;
-        inventoryManager.inventory = saveManager.inventoryManager.inventory;
+        totalPlayerScore = saveManager.SaveTotalScore;
+        totalPlayerGems = saveManager.SaveTotalGems;
+        totalPlayerCoins = saveManager.SaveTotalCoins;
+        highScore = saveManager.SaveHighScore;
+        
+        adjustMusicVolume = saveManager.MusicVolume;
+        toggleMusicMute = saveManager.MusicMute;
+        adjustSfxVolume = saveManager.SfxVolume;
+        toggleSfxMute= saveManager.SfxMute;
     }
-    
+
     [ContextMenu("Add Points")]
     private void AddGPoints()
     {
         totalPlayerScore += 10;
     }
-    
+
     [ContextMenu("Add Gems")]
     private void AddGems()
     {
@@ -74,13 +85,6 @@ public class GameTester : MonoBehaviour
         totalPlayerCoins += 5;
     }
 
-    [ContextMenu("Add Items")]
-    private void AddItems()
-    {
-        inventoryManager.AddItem(new InventoryItem("Board", 1));
-        inventoryManager.AddItem(new InventoryItem("Wheel", 4));
-    }
-    
     [ContextMenu("End Run")]
     private void EndRun()
     {
@@ -94,8 +98,36 @@ public class GameTester : MonoBehaviour
     private void ResetPlayerData()
     {
         totalPlayerScore = totalPlayerGems = totalPlayerCoins = currentRunScore = highScore = 0;
-        inventoryManager.ClearInventory();
-        saveManager.SavedHighScore = 0;
+        saveManager.SaveHighScore = 0;
+        adjustMusicVolume = 0.5f;
+        toggleMusicMute = true;
+        adjustSfxVolume = 0.5f;
+        toggleSfxMute= true;
         SavePlayerData();
+    }
+
+    // Context menus for adjusting music and sound effect settings
+    [ContextMenu("Set Music Volume 50%")]
+    private void SetMusicVolume50()
+    {
+        adjustMusicVolume += 0.3f;
+    }
+
+    [ContextMenu("Set Music Volume 0% (Mute)")]
+    private void SetMusicMute()
+    {
+        toggleMusicMute = false;
+    }
+
+    [ContextMenu("Set Music Volume 100%")]
+    private void SetMusicVolume100()
+    {
+        adjustSfxVolume += 0.03f;
+    }
+
+    [ContextMenu("Set Music Volume 0% (Mute)")]
+    private void SetSfxMute()
+    {
+        toggleSfxMute = false;
     }
 }
--- EndlessRunner/Assets/Scripts/SaveManager.cs
+++ EndlessRunner/Assets/Scripts/SaveManager.cs
@@ -1,112 +1,76 @@
-using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
-    private int savedPlayerScore;
-    private int savedPlayerGems;
-    private int savedPlayerCoins;
+    public int SaveTotalScore { get; set; }
+    public int SaveTotalGems { get; set; }
+    public int SaveTotalCoins { get; set; }
+    public int SaveHighScore { get; set; }
 
-    private float musicVolume = 1f;
-    private bool musicMute = false;
-    private float sfxVolume = 1f;
-    private bool sfxMute = false;
+    public float MusicVolume { get; set; }
+    public bool MusicMute { get; set; }
+    public float SfxVolume { get; set; }
+    public bool SfxMute { get; set; }
 
-    public InventoryManager inventoryManager;
-
-    public int SavedPlayerScore { get; set; }
-    public int SavedPlayerGems { get; set; }
-    public int SavedPlayerCoins { get; set; }
-    public int SavedHighScore { get; set; }
-
-    public float MusicVolume
+    private void Awake()
     {
-        get { return musicVolume; }
-        set
-        {
-            musicVolume = Mathf.Clamp01(value);
-            AudioListener.volume = musicVolume;
-        }
+        LoadData();
     }
-
-    public bool MusicMute
-    {
-        get { return AudioListener.pause; }
-        set { AudioListener.pause = value; }
-    }
-
-    public float SFXVolume
-    {
-        get { return sfxVolume; }
-        set
-        {
-            sfxVolume = Mathf.Clamp01(value);
-            AudioListener.volume = sfxVolume;
-        }
-    }
-
-    public bool SFXMute
-    {
-        get { return sfxMute; }
-        set { sfxMute = value; }
-    }
-
-    private void Start() => LoadData();
 
     public void LoadData()
     {
         string path = Application.persistentDataPath + "/save.json";
         if (!File.Exists(path)) return;
+
         string json = File.ReadAllText(path);
-        SaveData saveData = JsonUtility.FromJson<SaveData>(json);
-        SavedPlayerScore = saveData.playerScore;
-        SavedPlayerGems = saveData.playerGems;
-        SavedPlayerCoins = saveData.playerCoins;
-        SavedHighScore = saveData.playerHighScore;
-        inventoryManager.inventory = saveData.inventory;
-        musicVolume = saveData.musicVolume;         // Update music settings
-        AudioListener.volume = musicVolume;
-        musicMute = saveData.musicMute;
-        AudioListener.pause = musicMute;
-        sfxVolume = saveData.sfxVolume;         // Update SFX settings
-        AudioListener.volume = sfxVolume;
-        sfxMute = saveData.sfxMute;
-    }
+        GameData data = JsonUtility.FromJson<GameData>(json);
 
-    private void SaveData()
-    {
-        SaveData saveData = new SaveData
-        {
-            playerScore = SavedPlayerScore,
-            playerGems = SavedPlayerGems,
-            playerCoins = SavedPlayerCoins,
-            playerHighScore = SavedHighScore,
-            inventory = inventoryManager.inventory,
-            musicVolume = musicVolume,             // Save music settings
-            musicMute = musicMute,
-            sfxVolume = sfxVolume,             // Save SFX settings
-            sfxMute = sfxMute,
-        };
+        SaveTotalScore = data.playerScore;
+        SaveTotalGems = data.playerGems;
+        SaveTotalCoins = data.playerCoins;
+        SaveHighScore = data.playerHighScore;
 
-        string json = JsonUtility.ToJson(saveData);
-        File.WriteAllText(Application.persistentDataPath + "/save.json", json);
+        MusicVolume = data.musicVolume;
+        MusicMute = data.musicMute;
+        SfxVolume = data.sfxVolume;
+        SfxMute = data.sfxMute;
+
+        UpdateAudioSettings();
     }
 
     public void SaveGameData()
     {
-        SaveData();
+        GameData data = new GameData
+        {
+            playerScore = SaveTotalScore,
+            playerGems = SaveTotalGems,
+            playerCoins = SaveTotalCoins,
+            playerHighScore = SaveHighScore,
+            musicVolume = MusicVolume,
+            musicMute = MusicMute,
+            sfxVolume = SfxVolume,
+            sfxMute = SfxMute,
+        };
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/save.json", json);
+    }
+
+    private void UpdateAudioSettings()
+    {
+        AudioListener.volume = (MusicMute) ? 0f : MusicVolume;
+        AudioListener.pause = MusicMute;
     }
 }
 
 [System.Serializable]
-public class SaveData
+public class GameData
 {
     public int playerScore;
     public int playerGems;
     public int playerCoins;
     public int playerHighScore;
-    public List<InventoryItem> inventory;
     public float musicVolume;
     public bool musicMute;
     public float sfxVolume;
