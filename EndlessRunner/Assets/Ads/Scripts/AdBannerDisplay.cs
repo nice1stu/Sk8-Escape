ba72e79b7c0d844a94db7b74ae7541e1a2bad1ba
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-05-04 15:04:12 +0200

    #400 Fixes errors with Banner script

--- EndlessRunner/Assets/Ads/Scripts/AdBannerDisplay.cs
+++ EndlessRunner/Assets/Ads/Scripts/AdBannerDisplay.cs
@@ -1,9 +1,10 @@
+using System;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
 namespace Ads.Scripts
 {
-    public class AdBannerDisplay : MonoBehaviour
+    public class AdBannerDisplay : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
     {
         //The game id to access ads
         public string myGameIdAndroid = "5266579";
@@ -16,11 +17,9 @@ namespace Ads.Scripts
         //If you want to be in test mode or not
         public bool testMode = true;
 
-        private void Start()
+        private void Awake()
         {
-            //Initialize the ad
-            Advertisement.Initialize(myGameIdAndroid, testMode);
-            myAdUnitId = adUnitIdAndroid;
+            InitializeAds();
         }
 
         private void Update()
@@ -28,10 +27,76 @@ namespace Ads.Scripts
             //Run the ad
             if (Advertisement.isInitialized && !adStarted)
             {
-                Advertisement.Load(myAdUnitId);
-                Advertisement.Show(myAdUnitId);
-                adStarted = true;
+                LoadAd();
             }
+        }
+
+        public void InitializeAds()
+        {
+            myAdUnitId = adUnitIdAndroid;
+            if (!Advertisement.isInitialized && Advertisement.isSupported)
+            {
+                Advertisement.Initialize(myGameIdAndroid, testMode, this);
+            }
+        }
+        
+        // Load content to the Ad Unit:
+        public void LoadAd()
+        {
+            // IMPORTANT! Only load content AFTER initialization (in this example, initialization is handled in a different script).
+            Debug.Log("Loading Ad: " + myAdUnitId);
+            Advertisement.Load(myAdUnitId, this);
+        }
+        
+        // Show the loaded content in the Ad Unit:
+        public void ShowAd()
+        {
+            // Note that if the ad content wasn't previously loaded, this method will fail
+            Debug.Log("Showing Ad: " + myAdUnitId);
+            Advertisement.Show(myAdUnitId, this);
+        }
+
+        public void OnInitializationComplete()
+        {
+            Debug.Log("Unity Ads initialization complete.");
+
+        }
+
+        public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+        {
+            Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");        
+        }
+
+        public void OnUnityAdsAdLoaded(string placementId)
+        {
+            ShowAd();
+        }
+
+        public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+        {
+            Debug.Log($"Error loading Ad Unit: {myAdUnitId} - {error.ToString()} - {message}");
+            // Optionally execute code if the Ad Unit fails to load, such as attempting to try again.
+        }
+
+        public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+        {
+            Debug.Log($"Error showing Ad Unit {myAdUnitId}: {error.ToString()} - {message}");
+            // Optionally execute code if the Ad Unit fails to show, such as loading another ad.
+        }
+
+        public void OnUnityAdsShowStart(string placementId)
+        {
+            adStarted = true;
+        }
+
+        public void OnUnityAdsShowClick(string placementId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+        {
+            throw new NotImplementedException();
         }
     }
 }
