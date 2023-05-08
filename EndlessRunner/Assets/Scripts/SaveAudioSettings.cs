b6bd239e91f3b1be91837790fd7253ac97ffa673
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-05-08 21:48:10 +0200

    #414 Fix Settings Sliders
    refactored code to remove Rider warnings

--- EndlessRunner/Assets/Scripts/SaveAudioSettings.cs
+++ EndlessRunner/Assets/Scripts/SaveAudioSettings.cs
@@ -13,21 +13,21 @@ public class SaveAudioSettings : MonoBehaviour
     public AudioMixer sfxMixer;
     public string musicMixerVolumeParameter = "MasterVolume";
     public string sfxMixerVolumeParameter = "MasterVolume";
-    public bool musicMuted = false;
-    public bool sfxMuted = false;
+    public bool musicMuted;
+    public bool sfxMuted;
 
     private float SaveMusicVolume { get; set; }
     private bool SaveMusicMute { get; set; }
-    public float SaveSfxVolume { get; set; }
-    public bool SaveSfxMute { get; set; }
-    public string SaveLanguageSetting { get; set; }
+    private float SaveSfxVolume { get; set; }
+    private bool SaveSfxMute { get; set; }
+    private string SaveLanguageSetting { get; set; }
 
     private void Awake()
     {
         LoadSettingsData();
     }
 
-    public void LoadSettingsData()
+    private void LoadSettingsData()
     {
         SaveMusicVolume = PlayerPrefs.GetFloat(MusicVolumeKey, 1.0f);
         SaveMusicMute = PlayerPrefs.GetInt(MusicMuteKey, 0) == 1;
@@ -51,26 +51,12 @@ public class SaveAudioSettings : MonoBehaviour
     {
         if (musicMixer != null)
         {
-            if (musicMuted)
-            {
-                musicMixer.SetFloat(musicMixerVolumeParameter, -80f);
-            }
-            else
-            {
-                musicMixer.SetFloat(musicMixerVolumeParameter, Mathf.Log10(SaveMusicVolume) * 20);
-            }
+            musicMixer.SetFloat(musicMixerVolumeParameter, musicMuted ? -80f : Mathf.Log10(SaveMusicVolume) * 20);
         }
 
         if (sfxMixer != null)
         {
-            if (sfxMuted)
-            {
-                sfxMixer.SetFloat(sfxMixerVolumeParameter, -80f);
-            }
-            else
-            {
-                sfxMixer.SetFloat(sfxMixerVolumeParameter, Mathf.Log10(SaveSfxVolume) * 20);
-            }
+            sfxMixer.SetFloat(sfxMixerVolumeParameter, sfxMuted ? -80f : Mathf.Log10(SaveSfxVolume) * 20);
         }
     }
 }
