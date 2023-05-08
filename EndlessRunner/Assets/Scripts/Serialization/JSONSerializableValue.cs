b32bab051eba3bde2cf6372b4ecd8964215671db
Hampus brink <112471046+Gandhopp@users.noreply.github.com>
2023-05-08 09:48:46 +0200

    custom paths for serializable values

--- EndlessRunner/Assets/Scripts/Serialization/JSONSerializableValue.cs
+++ EndlessRunner/Assets/Scripts/Serialization/JSONSerializableValue.cs
@@ -9,16 +9,16 @@ namespace Serialization
         [SerializeField] private SerializableValue coins;
         
         [ContextMenu("Save")]
-        private void Save()
+        private void Save(string pathName)
         {
             SerializableValue data = coins;
             string json = JsonUtility.ToJson(data);
-            File.WriteAllText(Application.persistentDataPath + "/inventory.save.json", json);
+            File.WriteAllText(Application.persistentDataPath + $"/{pathName}.save.json", json);
         }
         [ContextMenu("Load")]
-        private void Load()
+        private void Load(string pathName)
         {
-            string path = Application.persistentDataPath + "/inventory.save.json";
+            string path = Application.persistentDataPath + $"/{pathName}.save.json";
             if (!File.Exists(path)) return;
 
             string json = File.ReadAllText(path);
