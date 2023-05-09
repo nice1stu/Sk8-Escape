2a1559098c62c540986d33db0e3004c233c7b6f0
Emir Ulusoy <Emir.ulusoy@student.forsbergsskola.se>
2023-05-09 15:20:47 +0200

    #302 can update the database now
    now you can see updates in firebase database

--- EndlessRunner/Assets/Scripts/Database/DatabaseManager.cs
+++ EndlessRunner/Assets/Scripts/Database/DatabaseManager.cs
@@ -36,46 +36,59 @@ public class DatabaseManager : MonoBehaviour
         datareference.Child("users").Child(userID).SetRawJsonValueAsync(json);
     }
 
-    // // retrieves user's name from Firebase database and passes it to another function as string
-    // public IEnumerator GetName(Action<string> onCallback)
-    // {
-    //     var userNameData = datareference.Child("user").Child(userID).Child("name").GetValueAsync();
-    //     
-    //     // pauses the execution until database calls complete
-    //     yield return new WaitUntil(predicate: () => userNameData.IsCompleted);
-    //
-    //     if (userNameData != null)
-    //     {
-    //         DataSnapshot snapshot = userNameData.Result;
-    //
-    //         onCallback.Invoke(snapshot.Value.ToString());
-    //     }
-    // }
-    //
-    // public IEnumerator GetGold(Action<int> onCallback)
-    // {
-    //     var userGoldData = datareference.Child("user").Child(userID).Child("gold").GetValueAsync();
-    //
-    //     yield return new WaitUntil(predicate: () => userGoldData.IsCompleted);
-    //
-    //     if (userGoldData != null)
-    //     {
-    //         DataSnapshot snapshot = userGoldData.Result;
-    //
-    //         onCallback.Invoke(int.Parse(snapshot.Value.ToString()));
-    //     }
-    // }
-    //
-    // // should retrieves name and gold and display's information ( UI )
-    // public void GetUserInfo()
-    // {
-    //     StartCoroutine(GetName((string name) =>
-    //     {
-    //         NameText.text = "Name: " + name;
-    //     }));
-    //     StartCoroutine(GetGold((int gold) =>
-    //     {
-    //         GoldText.text = "Gold: " + gold.ToString();
-    //     }));
-    // }
+    // retrieves user's name from Firebase database and passes it to another function as string
+    public IEnumerator GetName(Action<string> onCallback)
+    {
+        var userNameData = datareference.Child("user").Child(userID).Child("name").GetValueAsync();
+        
+        // pauses the execution until database calls complete
+        yield return new WaitUntil(predicate: () => userNameData.IsCompleted);
+    
+        if (userNameData != null)
+        {
+            DataSnapshot snapshot = userNameData.Result;
+    
+            onCallback.Invoke(snapshot.Value.ToString());
+        }
+    }
+    
+    public IEnumerator GetGold(Action<int> onCallback)
+    {
+        var userGoldData = datareference.Child("user").Child(userID).Child("gold").GetValueAsync();
+    
+        yield return new WaitUntil(predicate: () => userGoldData.IsCompleted);
+    
+        if (userGoldData != null)
+        {
+            DataSnapshot snapshot = userGoldData.Result;
+    
+            onCallback.Invoke(int.Parse(snapshot.Value.ToString()));
+        }
+    }
+    
+    // should retrieves name and gold and display's information ( UI )
+    public void GetUserInfo()
+    {
+        StartCoroutine(GetName((string Username) =>
+        {
+            NameText.text = "Name: " + Username.ToString();
+        }));
+        StartCoroutine(GetGold((int gold) =>
+        {
+            GoldText.text = "Gold: " + gold.ToString();
+        }));
+    }
+
+    public void UpdateName()
+    {
+        datareference.Child("users").Child(userID).Child("name").SetValueAsync(Name.text);
+    }
+    public void UpdateGold()
+    {
+        datareference.Child("users").Child(userID).Child("gold").SetValueAsync(Gold.text);
+    }
+    public void UpdateSilver()
+    {
+        datareference.Child("users").Child(userID).Child("silver").SetValueAsync(Silver.text);
+    }
 }
