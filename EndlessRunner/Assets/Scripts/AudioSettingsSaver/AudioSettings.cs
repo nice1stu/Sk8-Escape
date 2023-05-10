1a2b91af8ded551f00467f309d204e1b9c2c57ce
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-05-10 18:24:25 +0200

    #414 Audio Settings Saver
    Enable persistent data by saving to json

--- EndlessRunner/Assets/Scripts/AudioSettingsSaver/AudioSettings.cs
+++ EndlessRunner/Assets/Scripts/AudioSettingsSaver/AudioSettings.cs
@@ -1,21 +1,65 @@
+using System;
+using System.IO;
+using UnityEngine;
+
 namespace AudioSettingsSaver
 {
-    public class AudioSettings : IAudioSettings
+    public abstract class AudioSettings : IAudioSettings
     {
-        private readonly IAudioChannelSettings _music;
-        private readonly AudioChannelSettings _sfx;
+        private AudioChannelSettings _music;
+        private AudioChannelSettings _sfx;
 
-        public AudioSettings()
+        private const string SettingsFilePath = "audio_settings.json";
+
+        protected AudioSettings()
         {
-            _music = new AudioChannelSettings();
-            _sfx = new AudioChannelSettings();
-
-            // Load default values
-            _music.Volume = 1f;
-            _sfx.Volume = 1f;
+            Load();
         }
 
         public IAudioChannelSettings Music => _music;
         public IAudioChannelSettings Sfx => _sfx;
+
+        public void Save()
+        {
+            // Serialize audio settings to JSON
+            AudioSettingsData data = new AudioSettingsData
+            {
+                musicVolume = _music.Volume,
+                musicMuted = _music.Muted,
+                sfxVolume = _sfx.Volume,
+                sfxMuted = _sfx.Muted
+            };
+            string json = JsonUtility.ToJson(data, true);
+
+            // Write JSON to file
+            File.WriteAllText(Path.Combine(Application.persistentDataPath, SettingsFilePath), json);
+        }
+
+        private void Load()
+        {
+            // Load audio settings from JSON file, or use default values if file doesn't exist
+            string filePath = Path.Combine(Application.persistentDataPath, SettingsFilePath);
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                AudioSettingsData data = JsonUtility.FromJson<AudioSettingsData>(json);
+                _music = new AudioChannelSettings { Volume = data.musicVolume, Muted = data.musicMuted };
+                _sfx = new AudioChannelSettings { Volume = data.sfxVolume, Muted = data.sfxMuted };
+            }
+            else
+            {
+                _music = new AudioChannelSettings { Volume = 1f, Muted = false };
+                _sfx = new AudioChannelSettings { Volume = 1f, Muted = false };
+            }
+        }
+
+        [Serializable]
+        private class AudioSettingsData
+        {
+            public float musicVolume;
+            public bool musicMuted;
+            public float sfxVolume;
+            public bool sfxMuted;
+        }
     }
 }
