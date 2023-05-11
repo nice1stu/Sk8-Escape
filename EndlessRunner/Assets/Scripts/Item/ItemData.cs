c98accc9bdcadde5a28b158d266d24cc06c9d321
Hampus brink <hampus.brink@student.forsbergsskola.se>
2023-05-11 12:27:33 +0200

    Hotfix for new stats

--- EndlessRunner/Assets/Scripts/Item/ItemData.cs
+++ EndlessRunner/Assets/Scripts/Item/ItemData.cs
@@ -20,12 +20,6 @@ namespace Item
 
         public IStats BonusStats => bonusStats;
 
-        public IStats TotalStats => new Stats
-        {
-            Stability = bonusStats.Stability + itemConfig.BaseStats.Stability,
-            Speed = bonusStats.Speed + itemConfig.BaseStats.Speed,
-            Style = bonusStats.Style + itemConfig.BaseStats.Style,
-            Balance = bonusStats.Balance + itemConfig.BaseStats.Balance
-        };
+        public IStats TotalStats => itemConfig.BaseStats.Add(bonusStats);
     }
 }
