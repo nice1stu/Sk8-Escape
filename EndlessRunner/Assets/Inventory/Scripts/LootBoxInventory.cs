78c9f20a41f3252cff14629d4e1a57cfc8d15bcc
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-05-11 16:24:14 +0200

    #502 Adds bool for shop, adds four slots to LBI

--- EndlessRunner/Assets/Inventory/Scripts/LootBoxInventory.cs
+++ EndlessRunner/Assets/Inventory/Scripts/LootBoxInventory.cs
@@ -1,16 +1,34 @@
 using System;
 using Item;
+using Unity.VisualScripting;
 
 namespace Inventory.Scripts
 {
     public class LootBoxInventory : ILootBoxInventory
     {
-        public int count;
+        public bool IsFull //Shop need this one to check if the loot box inventory is full or not
+        {
+            get
+            {
+                foreach (var lootBoxData in _slots)
+                {
+                    if (lootBoxData == null) return false;
+                }
+
+                return true;
+            }
+        }
+
         private ILootBoxData[] _slots;
 
         //This is the fixed array that stores the loot boxes
         public ILootBoxData[] Slots => _slots;
         //This is the list that stores the inventory slots
+
+        public LootBoxInventory()
+        {
+            _slots = new ILootBoxData[4];
+        }
         
         public void AddLootBox(ILootBoxData lootBox)
         {
@@ -20,7 +38,6 @@ namespace Inventory.Scripts
                 {
                     _slots[i] = lootBox;
                     LootBoxAdded?.Invoke(i, lootBox);
-                    count++;
                     return;
                 }
             }
@@ -32,7 +49,6 @@ namespace Inventory.Scripts
             if (slotIndex == -1) return;
             if (DateTime.UtcNow - lootBox.OpeningStartTime < lootBox.Config.TimeToOpen) return;
             _slots[slotIndex] = null;
-            count--;
             LootBoxRemoved?.Invoke(slotIndex, lootBox);
             //TODO: Use ItemFactory to create items
             LootBoxOpened?.Invoke(lootBox, Array.Empty<IItemData>());
