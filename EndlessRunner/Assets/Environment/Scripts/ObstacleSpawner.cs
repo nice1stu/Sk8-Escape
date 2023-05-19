a22cc32448787e9f95096c209e2dc0f69759cd8b
KaleSmug <karl.sandegren@gmail.com>
2023-05-19 10:34:23 +0200

    #006.5 Obstacle spawner made dynamic

--- EndlessRunner/Assets/Environment/Scripts/ObstacleSpawner.cs
+++ EndlessRunner/Assets/Environment/Scripts/ObstacleSpawner.cs
@@ -10,19 +10,72 @@ public class ObstacleSpawner : MonoBehaviour
     public GameObject[] powerUpPrefabs;
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.layer == 8 )
+        if (col.gameObject.layer == 10 )
         {
             SpawnObstacle();
             SpawnPowerUp();
         }
     }
 
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.layer == 7)
+        {
+            //transform.position += new Vector3(1, 0, 0);
+        }
+    }
+
+    private float GetObstacleWidth(GameObject obstacle)
+    {
+        float minX = Mathf.Infinity;
+        float maxX = Mathf.NegativeInfinity;
+
+
+        foreach (Transform child in obstacle.transform)
+        {
+            SpriteRenderer spriteRenderer = child.GetComponent<SpriteRenderer>();
+            
+            if (spriteRenderer != null)
+            {
+                var bounds = spriteRenderer.bounds;
+                Vector3 boundsMin = bounds.min;
+                Vector3 boundsMax = bounds.max;
+
+                if (boundsMin.x < minX)
+                    minX = boundsMin.x;
+
+                if (boundsMax.x > maxX)
+                    maxX = boundsMax.x;
+            }
+            //Yeah this is ugly but it works
+            foreach (Transform youngerChild in child.transform)
+            {
+                SpriteRenderer youngerSpriteRenderer = youngerChild.GetComponent<SpriteRenderer>();
+            
+                if (youngerSpriteRenderer != null)
+                {
+                    var bounds = youngerSpriteRenderer.bounds;
+                    Vector3 boundsMin = bounds.min;
+                    Vector3 boundsMax = bounds.max;
+
+                    if (boundsMin.x < minX)
+                        minX = boundsMin.x;
+
+                    if (boundsMax.x > maxX)
+                        maxX = boundsMax.x;
+                }
+            }
+        }
+
+        return maxX - minX;
+    }
     public void SpawnObstacle()
     {
-        
-        transform.position = transform.position + new Vector3(20, 0, 0);
-        Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)], transform.position,
+        GameObject obstacle = Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)], transform.position,
             transform.rotation);
+        Debug.Log(GetObstacleWidth(obstacle) +" is the bounds size");
+        obstacle.transform.position += new Vector3(GetObstacleWidth(obstacle), 0, 0);
+        transform.position += new Vector3(GetObstacleWidth(obstacle)+8, 0, 0);
     }
 
     public void SpawnPowerUp()
