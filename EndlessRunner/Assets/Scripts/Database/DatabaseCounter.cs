793baa71806831d9633bf579866618df2a4af845
Emir Ulusoy <Emir.ulusoy@student.forsbergsskola.se>
2023-05-19 11:04:48 +0200

    #302 testing script

--- EndlessRunner/Assets/Scripts/Database/DatabaseCounter.cs
+++ EndlessRunner/Assets/Scripts/Database/DatabaseCounter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,19 @@ using UnityEngine.UI;
 public class DatabaseCounter : MonoBehaviour
 {
     public Text counterText;
+
+    private float timer;
+
+    private int counting;
     // Start is called before the first frame update
     void Start()
     {
+        counterText.text = "0";
         
     }
-    
+
+    void MyFunction()
+    {
+        
+    }
 }
