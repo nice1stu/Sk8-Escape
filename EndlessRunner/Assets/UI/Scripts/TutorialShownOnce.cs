7e96f402b057da8c5ee5a7fa4d82d45609cd6c8a
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2023-05-24 01:21:08 +0200

    Adds logic to script, add script to prefab

--- EndlessRunner/Assets/UI/Scripts/TutorialShownOnce.cs
+++ EndlessRunner/Assets/UI/Scripts/TutorialShownOnce.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 
 public class TutorialShownOnce : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // if its false it should call setactive true then change it to true and save it so its only once when installed
+        if (PlayerPrefs.HasKey("tutorialShown")) return;
+        Debug.Log("kem eg hingað?");
+        gameObject.SetActive(true);
+        PlayerPrefs.SetString("tutorialShown", "true");
     }
-    /*
-    show tutorial on first start : monobehaviour
-    [6:52 PM]
-    void start()
-    [6:52 PM]
-    if(playerprefs.getkey("tutorialShown")) return;
-    [6:52 PM]
-    tutorial.SetActive(true);
-    [6:53 PM]
-    playerprefs.setstring("tutorialShown", "true");
-    */
+
 }
