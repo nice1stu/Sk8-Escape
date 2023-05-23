2be9f6441e625dc3be458138cf7132768a0e0e01
nihanbir <nihan.bir@gmail.com>
2023-05-23 16:05:06 +0300

    #307 Updates savemanager.cs to upload data to firebase

--- EndlessRunner/Assets/Scripts/SaveManager.cs
+++ EndlessRunner/Assets/Scripts/SaveManager.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Collections;
 using System.IO;
 using Inventory.Scripts;
 using UnityEngine;
+using Firebase.Database;
+using GooglePlayGames;
+using UI.Scripts;
+using UnityEngine.Serialization;
 
 public class SaveManager : MonoBehaviour
 {
@@ -9,12 +14,18 @@ public class SaveManager : MonoBehaviour
     public int SaveTotalGems { get; set; }
     public int SaveTotalCoins { get; set; }
     public int SaveHighScore { get; set; }
-
+    
+    
     //public Countdown[] currentBoxes = new Countdown[4]; for testing
     private Countdown[] LootBoxes => LoadLootBoxData();
-
+    
+    private long onlineTimeStamp;
+    private long localTimeStamp;
+    private GameData onlineData;
     private void Awake()
     {
+        FirebaseDatabase.DefaultInstance.RootReference.KeepSynced(true);
+        StartCoroutine(GetStats());
         LoadData();
     }
 
@@ -23,6 +34,7 @@ public class SaveManager : MonoBehaviour
         var data = lootBoxes;
         var json = JsonUtility.ToJson(data);
         File.WriteAllText(Application.persistentDataPath + "/lootBoxes.save.json", json);
+        FirebaseDatabase.DefaultInstance.RootReference.Child("lootBoxes").Child("player").SetRawJsonValueAsync(json);
     }
 
     private Countdown[] LoadLootBoxData()
@@ -38,37 +50,95 @@ public class SaveManager : MonoBehaviour
     public void LoadData()
     {
         var path = Application.persistentDataPath + "/stats.save.json";
-        if (!File.Exists(path)) return;
+       
+        if (File.Exists(path))
+        {
+            var json = File.ReadAllText(path);
+            var localData = JsonUtility.FromJson<GameData>(json);
+            localTimeStamp = localData.timeStamp;
 
-        var json = File.ReadAllText(path);
-        var data = JsonUtility.FromJson<GameData>(json);
+            //get the most up to date data stats
+            if (localTimeStamp > onlineTimeStamp)
+            {
+                LoadSaveFile(localData);
+            }
+            else
+            {
+                LoadSaveFile(onlineData);
+                UpdateUIAndSave();
+            }
+        }
+    }
 
+    private void LoadSaveFile(GameData data)
+    {
         SaveTotalScore = data.totalScore;
         SaveTotalGems = data.totalGems;
         SaveTotalCoins = data.totalCoins;
         SaveHighScore = data.playerHighScore;
     }
 
+    private void UpdateUIAndSave()
+    {
+        UIManager uiManager = FindObjectOfType<UIManager>();
+        if (uiManager) uiManager.UpdateUI();
+
+        //save the online data on device to keep it up to date
+        SaveGameData();
+    }
+
     public void SaveGameData()
     {
-        var data = new GameData
+        GameData data = new GameData
         {
+            username = PlayGamesPlatform.Instance.localUser.userName,
             totalScore = SaveTotalScore,
             totalGems = SaveTotalGems,
             totalCoins = SaveTotalCoins,
-            playerHighScore = SaveHighScore
+            playerHighScore = SaveHighScore,
+            timeStamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
         };
 
         var json = JsonUtility.ToJson(data);
         File.WriteAllText(Application.persistentDataPath + "/stats.save.json", json);
+        if(data.username != String.Empty) FirebaseDatabase.DefaultInstance.RootReference.Child("users").Child(data.username).SetRawJsonValueAsync(json);
+
+    }
+    
+    //get online stats
+    public IEnumerator GetStats()
+    {
+        yield return new WaitForSeconds(1);
+        var userData = FirebaseDatabase.DefaultInstance.RootReference.Child("users").Child(PlayGamesPlatform.Instance.localUser.userName).GetValueAsync();
+        yield return new WaitUntil(predicate: () => userData.IsCompleted);
+        
+        DataSnapshot snapshot = userData.Result;
+        if (snapshot != null && snapshot.Exists)
+        {
+            onlineData = JsonUtility.FromJson<GameData>(snapshot.GetRawJsonValue());
+            onlineTimeStamp = onlineData.timeStamp;
+            //if the local data doesnt exist load online data
+            if (localTimeStamp == 0)
+            {
+                LoadSaveFile(onlineData);
+                UpdateUIAndSave();
+            }
+            else LoadData();
+        }
+        else
+        {
+            StartCoroutine(GetStats());
+        }
     }
 }
 
 [Serializable]
 public class GameData
 {
+    public string username;
     public int totalScore;
     public int totalGems;
     public int totalCoins;
     public int playerHighScore;
+    public long timeStamp;
 }
