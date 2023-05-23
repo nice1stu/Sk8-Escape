5ce505ff2491390dfe65bdecdf440ffca5b56d14
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2023-05-23 20:10:29 +0200

    Adding scripts for TutorialShownOnce

---
+++ EndlessRunner/Assets/UI/Scripts/TutorialShownOnce.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialShownOnce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    /*
+    show tutorial on first start : monobehaviour
+    [6:52 PM]
+    void start()
+    [6:52 PM]
+    if(playerprefs.getkey("tutorialShown")) return;
+    [6:52 PM]
+    tutorial.SetActive(true);
+    [6:53 PM]
+    playerprefs.setstring("tutorialShown", "true");
+    */
+}
