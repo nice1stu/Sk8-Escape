5a0139b16985bd5901a18e762ecc616379250db7
Felix Berge <felix.berge@student.forsbergsskola.se>
2023-05-23 23:11:52 +0200

    Cleanup TMPLocalizationMenu code

--- EndlessRunner/Assets/Editor/TMPLocalizationStateMenu.cs
+++ EndlessRunner/Assets/Editor/TMPLocalizationStateMenu.cs
@@ -1,96 +1,95 @@
-using UnityEngine;
-using UnityEditor;
-using TMPro;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Codice.Client.Common;
 using Lean.Localization;
-using UnityEditor.SceneManagement;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TMPLocalizationStateMenu : EditorWindow {
-    private List<TMP_Text> tmpObjects = new();
-    private Vector2 scrollPos;
-    [MenuItem ("Window/TMP Localization State")]
-    public static void  ShowWindow () {
-        GetWindow(typeof(TMPLocalizationStateMenu));
-    }
+namespace Editor
+{
+    public class TMPLocalizationStateMenu : EditorWindow 
+    {
+        private readonly List<TMP_Text> _tmpObjects = new();
+        private Vector2 _scrollPos;
+        [MenuItem ("Window/TMP Localization State")]
+        public static void ShowWindow() => GetWindow(typeof(TMPLocalizationStateMenu));
 
-    void OnGUI () {
-        FetchTMPObjects();
+        private void OnGUI () {
+            FetchTMPObjects();
 
-        EditorGUILayout.BeginVertical();
-        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+            EditorGUILayout.BeginVertical();
+            _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos);
 
-        foreach (var tmpObj in tmpObjects)
-        {
-            EditorGUILayout.BeginHorizontal();
+            foreach (var tmpObj in _tmpObjects)
+            {
+                EditorGUILayout.BeginHorizontal();
 
             
-            bool hasTranslationComponent = tmpObj.GetComponent<LeanLocalizedTextMeshProUGUI>();
+                bool hasTranslationComponent = tmpObj.GetComponent<LeanLocalizedTextMeshProUGUI>();
             
-            EditorGUI.BeginDisabledGroup(true);
-            EditorGUILayout.Toggle(hasTranslationComponent, GUILayout.Width(15)); // toggle readonly anyways
-            EditorGUI.EndDisabledGroup();
+                EditorGUI.BeginDisabledGroup(true);
+                EditorGUILayout.Toggle(hasTranslationComponent, GUILayout.Width(15)); // toggle readonly anyways
+                EditorGUI.EndDisabledGroup();
             
-            // EditorGUILayout.LabelField(GetGameObjectPath(tmpObjects[i].gameObject), GUILayout.MaxWidth(400));
+                // EditorGUILayout.LabelField(GetGameObjectPath(tmpObjects[i].gameObject), GUILayout.MaxWidth(400));
 
-            (string goPath, string goName) = RSplitPathName( GetGameObjectPath(tmpObj.gameObject) );
+                (string goPath, string goName) = RSplitPathName( GetGameObjectPath(tmpObj.gameObject) );
             
-            EditorGUILayout.LabelField(goPath, GUILayout.MaxWidth(300));
-            EditorGUILayout.LabelField(goName);
+                EditorGUILayout.LabelField(goPath, GUILayout.MaxWidth(300));
+                EditorGUILayout.LabelField(goName);
             
-            // ------------------------------- Buttons
-            if (GUILayout.Button("Select", GUILayout.MaxWidth(70))) {
-                Selection.activeGameObject = tmpObj.gameObject;
-            }
-
-            EditorGUI.BeginDisabledGroup(hasTranslationComponent);
-            
-            if (GUILayout.Button("Add", GUILayout.MaxWidth(50))) {
-                if (!hasTranslationComponent) {
-                    Undo.AddComponent<TextMeshProUGUI>(tmpObj.gameObject).enabled = true;
+                // ------------------------------- Buttons
+                if (GUILayout.Button("Select", GUILayout.MaxWidth(70))) {
                     Selection.activeGameObject = tmpObj.gameObject;
                 }
-                else
-                {
-                    GUI.enabled = false;
+
+                EditorGUI.BeginDisabledGroup(hasTranslationComponent);
+            
+                if (GUILayout.Button("Add", GUILayout.MaxWidth(50))) {
+                    if (!hasTranslationComponent) {
+                        Undo.AddComponent<TextMeshProUGUI>(tmpObj.gameObject).enabled = true;
+                        Selection.activeGameObject = tmpObj.gameObject;
+                    }
+                    else
+                    {
+                        GUI.enabled = false;
+                    }
+                }
+
+                EditorGUI.EndDisabledGroup();
+                // </Buttons> -------------------------------
+            
+                EditorGUILayout.EndHorizontal();
+            }
+
+            EditorGUILayout.EndScrollView();
+            EditorGUILayout.EndVertical();
+        }
+
+        private void FetchTMPObjects() {
+            _tmpObjects.Clear();
+        
+            foreach (GameObject obj in SceneManager.GetActiveScene().GetRootGameObjects()) {
+                TMP_Text[] tmps = obj.GetComponentsInChildren<TMP_Text>(true);
+
+                foreach (TMP_Text tmp in tmps) {
+                    _tmpObjects.Add(tmp);
                 }
             }
-
-            EditorGUI.EndDisabledGroup();
-            // </Buttons> -------------------------------
-            
-            EditorGUILayout.EndHorizontal();
         }
 
-        EditorGUILayout.EndScrollView();
-        EditorGUILayout.EndVertical();
-    }
-
-    private void FetchTMPObjects() {
-        tmpObjects.Clear();
-        
-        foreach (GameObject obj in SceneManager.GetActiveScene().GetRootGameObjects()) {
-            TMP_Text[] tmps = obj.GetComponentsInChildren<TMP_Text>(true);
-
-            foreach (TMP_Text tmp in tmps) {
-                tmpObjects.Add(tmp);
-            }
+        private static string GetGameObjectPath(GameObject obj) {
+            return obj.transform.parent == null ? obj.name : GetGameObjectPath(obj.transform.parent.gameObject) + "/" + obj.name; 
         }
-    }
 
-    private string GetGameObjectPath(GameObject obj) {
-        return obj.transform.parent == null ? obj.name : GetGameObjectPath(obj.transform.parent.gameObject) + "/" + obj.name; 
-    }
-
-    // returns path "" if not containing /
-    private (string path, string name) RSplitPathName(string split)
-    {
-        int index = split.LastIndexOf("/");
-        return index == -1 ? 
-            ("", split) : // not found
-            (split[(index + 1)..], split[..index]);
+        // returns path "" if not containing /
+        private static (string path, string name) RSplitPathName(string split)
+        {
+            int index = split.LastIndexOf("/", StringComparison.Ordinal);
+            return index == -1 ? 
+                ("", split) : // not found
+                (split[(index + 1)..], split[..index]);
+        }
     }
 }
